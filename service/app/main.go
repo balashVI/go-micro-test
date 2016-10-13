5d48f74b037b181863984ec975a5938a1db1832a
Vitaliy Balash <vitaliy.balash@vakoms.com.ua>
2016-10-13 23:41:15 +0300

    Added test service

--- service/app/main.go
+++ service/app/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main(){
-	fmt.Println("service")
+	"github.com/micro/go-micro"
+	"github.com/balashVI/go-micro-test/proto"
+	"golang.org/x/net/context"
+)
+
+type HealthCheck struct{}
+
+func (hc HealthCheck) Ping(ctx context.Context, req *proto.PingRequest, rsp *proto.PingResponse) error{
+	rsp.Message = "Pong"
+	return nil
+}
+
+func main() {
+	// Create a new service
+	service := micro.NewService(
+		micro.Name("go.micro-test.healthcheck"),
+		micro.Version("latest"),
+	)
+
+	// Init will parse the command line flags
+	service.Init()
+
+	// Register handler
+	proto.RegisterHealthCheckHandler(service.Server(), new(HealthCheck))
+
+	// Run the server
+	if err := service.Run(); err != nil {
+		fmt.Println(err)
+	}
 }
